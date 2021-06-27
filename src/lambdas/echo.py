e5b910cc2f1556b53c4899a99ae8910968c87a6c
David Jetter <davidajetter@gmail.com>
2021-06-27 19:54:43 -0400

    add log

--- src/lambdas/echo.py
+++ src/lambdas/echo.py
@@ -1,11 +1,18 @@
+import json
 import logging
 from typing import Dict
 from src.lifecycle import startup
+
 startup()
 
 logger = logging.getLogger(__name__)
 
 
 def echo(event: Dict, context: Dict) -> Dict:
+
+    logger.debug(
+        "Received event",
+        extra={"event": json.dumps(event), "request_id": context["aws_request_id "]},
+    )
 
     return event
