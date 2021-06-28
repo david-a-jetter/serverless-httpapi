b45dc0cd92df09880ce16a494a89daa791f8bb55
David Jetter <davidajetter@gmail.com>
2021-06-28 11:00:52 -0400

    remove default lambda log handler

--- hearty/lifecycle.py
+++ hearty/lifecycle.py
@@ -4,8 +4,8 @@ from pythonjsonlogger import jsonlogger
 
 def startup() -> None:
     logger = logging.getLogger()
+    [logger.removeHandler(h) for h in logger.handlers]
     logger.setLevel(logging.INFO)
     logHandler = logging.StreamHandler()
-    formatter = jsonlogger.JsonFormatter()
-    logHandler.setFormatter(formatter)
+    logHandler.setFormatter(jsonlogger.JsonFormatter())
     logger.addHandler(logHandler)
