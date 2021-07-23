9ec7193c3c17b1896782e75f6db3df7ad5fdc3f5
David Jetter <davidajetter@gmail.com>
2021-07-23 11:17:53 -0400

    add time series model

--- hearty/models.py
+++ hearty/models.py
@@ -1,6 +1,19 @@
+from typing import Optional
+
 from pydantic import BaseModel, EmailStr
 
 
 class Patient(BaseModel):
     id: str
     email: EmailStr
+
+
+class TimeSeriesData(BaseModel):
+    start_epoch_seconds: int
+    end_epoch_seconds: int
+    max: Optional[float]
+    mean: Optional[float]
+    median: Optional[float]
+    min: Optional[float]
+    standard_deviation: Optional[float]
+    sample_count: Optional[int]
